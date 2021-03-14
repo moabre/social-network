6172de8f3b44071e4b9b3ce1618f2c1618af9e61
moabre <muhammadrehman21@gmail.com>
2021-03-14 00:45:03 -0600

    changes to make deployment send files via server

--- server/server.js
+++ server/server.js
@@ -40,4 +40,13 @@ app.use('/', users);
 app.use('/', posts);
 app.use('/', auth);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
+
 app.listen(PORT, () => console.log(`server up and running on ${PORT} `));
